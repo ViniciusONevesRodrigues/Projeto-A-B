eac02b4c9a283d68f6fc4e1cdea46e874198912d
Vinicius O. das Neves Rodrigues <126347863+ViniciusONevesRodrigues@users.noreply.github.com>
2024-11-13 18:54:55 -0300

    Update App.js

--- versão A/src/App.js
+++ versão A/src/App.js
@@ -6,7 +6,7 @@ import Aside from './components/Aside';
 import Footer from './components/Footer';
 import Clarity from '@microsoft/clarity';
 
-Clarity.init('oy2xoen3fo');
+Clarity.init('oy47toq6hv');
 
 function App() {
   return (
