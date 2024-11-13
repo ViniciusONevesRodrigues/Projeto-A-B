0f517b8a12ec2480410a47029ac9464b6cc98bc8
Vinicius O. das Neves Rodrigues <126347863+ViniciusONevesRodrigues@users.noreply.github.com>
2024-11-13 20:33:08 +0000

    clarity

--- versão A/src/App.js
+++ versão A/src/App.js
@@ -6,7 +6,7 @@ import Aside from './components/Aside';
 import Footer from './components/Footer';
 import { clarity } from 'react-microsoft-clarity';
 
-if (process.env.NODE_ENV === 'production') clarity.init('oy2xoen3fo');
+clarity.init('oy2xoen3fo');
 
 function App() {
   return (
